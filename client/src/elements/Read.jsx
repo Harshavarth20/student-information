823b6117b39082cea8dffe47d8a08a216ff4dcc8
Harshavarth <harshavarthvengat@gmail.com>
2025-03-06 12:04:38 +0530

    initial commit

---
+++ client/src/elements/Read.jsx
@@ -0,0 +1,56 @@
+import React,{useState,useEffect} from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+
+function Read(){
+  const [data,setData] = useState([]);
+  const { id } = useParams();
+  useEffect(()=>{
+    axios
+      .get(`http://localhost:3001/get_student/${id}`)
+      .then((res) => {
+      setData(res.data);
+      })
+      .catch((err) => console.log(err));
+  },[id]);
+
+  return (
+    <div className="container-fluid vw-100 vh-100 bg-primary">
+      <h1>User </h1>
+
+    {data.map((student)=>{
+      return (
+      <ul className="list-group">
+        <li className="list-group-item">  
+          <b>ID: </b>
+          {student["id"]}
+        </li>
+        <li className="list-group-item">  
+          <b>Name: </b>
+          {student["name"]}
+        </li>
+        <li className="list-group-item">  
+          <b>Email: </b>
+          {student["email"]}
+        </li>
+        <li className="list-group-item">  
+          <b>Age: </b>
+          {student["age"]}
+        </li>
+        <li className="list-group-item">  
+          <b>Gender: </b>
+          {student["gender"]}
+        </li>
+      </ul>
+     );
+    })}
+    <br/>
+      <Link to='/'className="btn btn-success">
+        Back
+      </Link>
+    </div>
+  );
+}
+
+
+export default Read
